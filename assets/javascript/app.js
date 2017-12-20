cd8c16534807d376ed313690a22aa0b7f45777d2
Joseph-Marlitt <joseph.marlitt@gmail.com>
2017-12-20 12:52:10 -0600

    draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,31 +5,25 @@ var radius;
 var options;
 var map;
 var markers = [];
+//array for NAN coordinates on the NPS array
 var badMarkers= [];
 var mapCenter;
 
     //Search on-click reveals the map and column with information.
 $('#submitButton').on('click', function() {
+    //getting variables from state selector input
     var state = $('#state').val().trim();
     markers = [];
     var infoWindow = new google.maps.InfoWindow();
     //Display Map
-
     $('#map').css({
         'display': 'inline-block',
         'width': '100%',
         'height': '400px',
         'border': 'solid',
-        // 'border-left': 'none',
         'margin-top': '20px'
         });
-    // $('#infoColumn').css({
-    //     'display': 'inline-block',
-    //     'width': '100%',
-    //     'height': '400px',
-    //     'border': 'solid',
-    //     'margin-top': '20px'
-    // })
+    //Ready images for click on icons
     $('#images').css({
         'display': 'inline-block',
         'width': '100%',
@@ -37,6 +31,7 @@ $('#submitButton').on('click', function() {
         'margin-top': '20px',
         'overflow': 'scroll'
     })
+    //animation effects and scroll
     $('html, body').animate({
         scrollTop: $('#searchForm').offset().top
     }, 1000);
@@ -46,7 +41,6 @@ $('#submitButton').on('click', function() {
     $('#infoWindowHeader').css({
         'color': '#fcbd20;'
     })
-
     //Search result gets passed through NPS/ API.
     var settings = {
         "async": true,
@@ -59,7 +53,6 @@ $('#submitButton').on('click', function() {
             "postman-token": "509a7a9e-dd99-d214-70d9-793c94ad5033"
         }
     }
-
     //AJAX response for NPS and Instagram
     $.ajax(settings).done(function(response) {
         console.log(response);
@@ -68,38 +61,29 @@ $('#submitButton').on('click', function() {
         var latitude
         var longitude
         var locationName
-
         $.each(results, function(index, value) {
-
+            //finding the latitude and longitude
             var latLong = results[index].latLong
-
             //Get the location name
             locationName = results[index].fullName;
-            
-
             // Slice the string to break out latLong into two values.
             latitude = parseFloat(latLong.slice(latLong.indexOf(':') + 1, latLong.indexOf(',')));
             longitude = parseFloat(latLong.slice(latLong.lastIndexOf(':') + 1));
-
-        
-
             //  add markers/ from results
             var currentMarker = {
                     coords: { lat: latitude, lng: longitude},
                     content: '<h1>' + locationName +  '</h1>',
                     flickrImg: locationName
                 };
-
+            //Checking to see if the park has coordinates to pass through to our Array
             if (isNaN(latitude)) {
+                //push NAN markers to garbage array
                 badMarkers.push(currentMarker);
                 console.log(locationName + 'does not have latitude');
             } else {
+                // Push the markers into the array
                 markers.push(currentMarker);
             }
-
-            // Push the markers into the array
-            
-
         });
         mapCenter = markers[2].coords;
         console.log(mapCenter);
--- assets/javascript/gmaps.js
+++ assets/javascript/gmaps.js
@@ -1,15 +1,20 @@
+//Initialize the Google Map
 var initMap = function() {
+    //Centers on the state which is chosen.
     var options = {
         center: mapCenter,
         zoom: 5
     };
+    //places map in Div
     map = new google.maps.Map(
         document.getElementById('map'),
         options
     );
 }
-var addMarker = function(markers) {
+//Adding the markers function
 
+var addMarker = function(markers) {
+    //Add marker for each park in the array, adding content and creating a new marker for our map.
     for (var i = 0; i < markers.length; i++) {
         var content = '<div id="iw-content"' + markers[i].content + '</div>'
         var marker =  new google.maps.Marker({
@@ -17,16 +22,16 @@ var addMarker = function(markers) {
                 content: content,
                 map:map
         });
-        console.log(markers[i].flickrImg);
+        //Creating the infoWindows for the google map icons.
         var infoWindow = new google.maps.InfoWindow();
-
+        //listeners on the markers for hover and click.
         (function(marker, i) {
-            //Mouseover icon and reveal the title
+            //Mouseover icon and reveal the Park name
             marker.addListener('mouseover', function() {
                     infoWindow.setContent(marker.content);
                     infoWindow.open(map, marker);
             });
-            // add click event
+            // add click event for images to display in paralell div.
             google.maps.event.addListener(marker, 'click', function() {
                 infoWindow.setContent(marker.content);
                 infoWindow.open(map, marker);
@@ -38,6 +43,7 @@ var addMarker = function(markers) {
                     'border': 'solid',
                     'margin-top': '20px'
                 })
+                //Adds a title to the div so we know where images are from when viewing
                 $('#title').html('<div id="infoWindowHeader">' + marker.content + '</div>')
 
                 //Tell flickr what URL to get images from.
@@ -45,7 +51,7 @@ var addMarker = function(markers) {
                 console.log(markers[i].flickrImg);
                 
 
-                // assuming you also want to hide the infowindow when user mouses-out
+                // hide the infowindow when user mouses-out
                 marker.addListener('mouseout', function() {
                     infoWindow.close();
                 });
